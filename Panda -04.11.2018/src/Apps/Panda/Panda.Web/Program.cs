84958c69dd0b99123ca94895fb9597f7dd8f5404
PiBoyadzhieva <pikoleva@gmail.com>
2020-02-15 01:45:03 +0200

    Make Db Models Panda

--- Panda -04.11.2018/src/Apps/Panda/Panda.Web/Program.cs
+++ Panda -04.11.2018/src/Apps/Panda/Panda.Web/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using SIS.MvcFramework;
 
 namespace Panda.Web
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebHost.Start(new Startup());
         }
     }
 }
